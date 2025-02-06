0b35679f8c193a96f20dcf7b52d1f253a45f6991
admin <admin@Timofeys-MacBook-Air.local>
2025-02-06 16:37:52 +0200

    2page

--- index.js
+++ index.js
@@ -109,7 +109,43 @@ document.addEventListener("DOMContentLoaded", function () {
                 УЗД судин
             </div>
             <div class="text20 main-container__content2__content__science-data">
-                01.11.2024
+               <span>01.11.2024</span>
+                <img class='icon' src="images/icons/download.svg" alt="download"/>
+            </div>
+        </div>
+        <div class="box content text20 main-container__content2__content">
+            <div class="main-container__content2__content__science-name" >
+                УЗД судин
+            </div>
+            <div class="text20 main-container__content2__content__science-data">
+                <span>01.11.2024</span>
+                <img class='icon' src="images/icons/download.svg" alt="download"/>
+            </div>
+        </div>
+        <div class="box content text20 main-container__content2__content">
+            <div class="main-container__content2__content__science-name" >
+                УЗД судин
+            </div>
+            <div class="text20 main-container__content2__content__science-data">
+                <span>01.11.2024</span>
+                <img class='icon' src="images/icons/download.svg" alt="download"/>
+            </div>
+        </div>
+        <div class="box content text20 main-container__content2__content">
+            <div class="main-container__content2__content__science-name" >
+                УЗД судин
+            </div>
+            <div class="text20 main-container__content2__content__science-data">
+                <span>01.11.2024</span>
+                <img class='icon' src="images/icons/download.svg" alt="download"/>
+            </div>
+        </div>
+        <div class="box content text20 main-container__content2__content">
+            <div class="main-container__content2__content__science-name" >
+                УЗД судин
+            </div>
+            <div class="text20 main-container__content2__content__science-data">
+               <span>01.11.2024</span>
                 <img class='icon' src="images/icons/download.svg" alt="download"/>
             </div>
         </div>
